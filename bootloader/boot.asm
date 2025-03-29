edcb3144ec94266760d5550f65104f599c3c41b1
hacimertgokhan <hacimertgokhan@gmail.com>
2025-03-29 17:09:32 +0300

    0.0.1

---
+++ bootloader/boot.asm
@@ -0,0 +1,60 @@
+BITS 16
+ORG 0x7C00
+
+start:
+    cli
+    mov si, boot_msg
+    call print_string
+
+    ; Protected Mode'a Geçiş
+    lgdt [gdt_desc]
+    mov eax, cr0
+    or  eax, 1
+    mov cr0, eax
+    jmp CODE_SEG:init_protected_mode
+
+print_string:
+    mov ah, 0x0E
+.loop:
+    lodsb
+    or al, al
+    jz .done
+    int 0x10
+    jmp .loop
+.done:
+    ret
+
+[SECTION .bss]
+align 16
+stack resb 4096
+
+[SECTION .data]
+boot_msg db "Rust Kernel Loading...", 0
+gdt_start:
+    dq 0x0000000000000000  ; NULL Descriptor
+    dq 0x00CF9A000000FFFF  ; Code Segment Descriptor
+    dq 0x00CF92000000FFFF  ; Data Segment Descriptor
+gdt_end:
+gdt_desc:
+    dw gdt_end - gdt_start - 1
+    dd gdt_start
+
+CODE_SEG equ gdt_start - gdt_start
+DATA_SEG equ gdt_start - gdt_start + 8
+
+[SECTION .text]
+BITS 32
+init_protected_mode:
+    mov ax, DATA_SEG
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax
+    mov esp, stack + 4096
+    call rust_kernel
+    cli
+    hlt
+
+rust_kernel:
+    jmp 0x100000
---
+++ src/main.rs
@@ -0,0 +1,20 @@
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+#[unsafe(no_mangle)]
+pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+    unsafe {
+        *vga_buffer = b'H';
+        *vga_buffer.offset(1) = 0x0F;
+    }
+
+    loop {}
+}
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
